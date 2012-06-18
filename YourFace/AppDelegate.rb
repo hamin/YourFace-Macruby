4f0a09a44be856418056b1a73bc094df4c5e3158
Haris Amin <aminharis7@gmail.com>
2012-06-18 18:37:32 -0400

    Initial Commit

---
+++ YourFace/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  YourFace
+#
+#  Created by Haris Amin on 6/18/12.
+#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ YourFace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YourFace
+//
+//  Created by Haris Amin on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
