db07e203d95c217f1f0263c845c08107fd22a19f
Haris Amin <aminharis7@gmail.com>
2012-06-21 02:25:33 -0400

    added takePicture action which also makes Face.com recognize request

--- YourFace/AppDelegate.rb
+++ YourFace/AppDelegate.rb
@@ -6,8 +6,10 @@
 #  Copyright 2012 __MyCompanyName__. All rights reserved.
 #
 
+require 'face'
+
 class AppDelegate
-    attr_accessor :window, :camera_preview
+    attr_accessor :window, :camera_preview, :picture_output, :recognized_label
     
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
@@ -26,6 +28,13 @@ class AppDelegate
         
         queue = Dispatch::Queue.new('cameraQueue')
         output.setSampleBufferDelegate self, queue:queue.dispatch_object
+        
+        
+        # Picture output settings
+        @picture_output = AVCaptureStillImageOutput.new
+        output_settings = {AVVideoCodecKey => AVVideoCodecJPEG}
+        @picture_output.outputSettings = output_settings
+        session.addOutput @picture_output
         
         #output.setVideoSettings KCVPixelBufferPixelFormatTypeKey => KCVPixelFormatType_32BGRA, KCVPixelBufferWidthKey => width, KCVPixelBufferHeightKey => height
         
@@ -59,10 +68,41 @@ class AppDelegate
         features = @detector.featuresInImage(image)
         
         features.each do |feature|
-            NSLog "Feature left - #{feature.leftEyePosition}"
+            #NSLog "Feature left - #{feature.leftEyePosition}"
         end
         
         nil
+    end
+    
+    def takePicture(sender)
+        img_connection = @picture_output.connectionWithMediaType AVMediaTypeVideo
+        NSLog "IT CAME TO TAKE PICTURE"
+        
+        call_back = Proc.new do |img_buffer, error|
+            NSLog "IT CAME TO CALLBACK"
+            image_data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation img_buffer
+            filename = '/Users/haris.amin/Desktop/YourFace/haris_test_pic.jpeg'
+            image_data.writeToFile(filename, atomically:true)
+            
+            key = '' 
+            secret ''
+            
+            client = Face.get_client(:api_key => key, :api_secret => secret)
+            resp = client.faces_recognize(:uids => ['all@aminharis7'], :file => File.new(filename, 'rb'), :detector => "normal")
+            
+            if resp.nil?
+                NSLog("IT IS NIL")
+                @recognized_label.stringValue = "NOTHING"
+            else
+                NSLog("IT HAS SOMETHING")
+                NSLog(resp.description)
+                @recognized_label.stringValue = "SOMETHING"
+                #@recognized_label.stringValue = resp['photos'].first["tags"].first["label"]
+            end
+            
+        end
+        
+        @picture_output.captureStillImageAsynchronouslyFromConnection img_connection, completionHandler:call_back
     end    
 end
 
